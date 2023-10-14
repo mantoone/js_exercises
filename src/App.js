5b0a8fd24e7b0ca74f1bbb10c7db0555d24591ec
mantoone <mantoone@icloud.com>
2023-10-14 11:43:34 +0300

    First commit

---
+++ src/App.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import CodeMirror from '@uiw/react-codemirror';
+import { javascript } from '@codemirror/lang-javascript';
+import './App.css';
+
+function App() {
+  const [value, setValue] = React.useState("console.log('hello world!');");
+  const [errors, setErrors] = React.useState("");
+  const onChange = React.useCallback((val, viewUpdate) => {
+    setValue(val);
+  }, []);
+
+  const onClick = React.useCallback(() => {
+    try {
+      eval(value);
+      setErrors("");
+    } catch (err) {
+      console.log('err:', err);
+      const matches = err.stack.match(/<anonymous>:(\d+):(\d+)/g);
+      let errorString = '';
+      if (matches) {
+        matches.forEach((match) => {
+          const [, line, column] = match.match(/<anonymous>:(\d+):(\d+)/);
+          if (errorString){
+            errorString += '\tcall from ';
+          } else {
+            errorString += ' on '
+          }
+          errorString += `line: ${line}, column: ${column}\n`;
+        });
+        setErrors(errorString);
+      } 
+      setErrors(err.toString() + errorString);
+    }
+  }, [value]);
+
+  return (
+    <div className="App">
+      <header>
+        <nav>
+          <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Contact</a></li>
+          </ul>
+        </nav>
+      </header>
+      <CodeMirror value={value} height="200px" extensions={[javascript({ jsx: true })]} onChange={onChange} />
+      {errors ? <pre>{errors}</pre> : null}
+      <button style={{marginTop: '10px'}} onClick={onClick}>Run</button>
+    </div>
+  );
+}
+
+export default App;
